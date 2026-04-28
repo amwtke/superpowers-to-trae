8711749e4f0e9d766b2a0d6ad1182f80a2df28ac
xiaojin <amwtke@gmail.com>
2026-04-28 16:54:32 +0800

    feat(cli): addons stub trait + handle_addons

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -6,6 +6,7 @@ use clap::{Parser, Subcommand};
 mod embed;
 mod agents_md;
 mod rollback;
+mod addons;
 
 #[derive(Parser, Debug)]
 #[command(name = "superpowers-trae", version, about, long_about = None)]
